723525bbf7cfbae48048a90c189f19b81810fa06
elijah84 <assefa.belaye@gmail.com>
2017-10-11 16:55:58 -0700

    Initial commit

---
+++ app/src/main/java/com/example/home/screenstayawake/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.home.screenstayawake;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+    }
+}
